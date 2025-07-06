646b8514adbf10ca858cb62cef75d5fce4680e2c
Kevin Qu <quchunguang@gmail.com>
2025-07-06 17:21:59 +0800

    add neovim with nvchad settings in `~/.config/nvim` for me

---
+++ nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+local o = vim.o
+o.tabstop = 8
+o.shiftwidth = 8
+o.softtabstop = 8
+o.relativenumber = true
+-- o.expandtab = false
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+      }
+    },
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+
+}
