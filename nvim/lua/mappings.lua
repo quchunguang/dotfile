b52ccbfd9d11a5c9720acada06edba47d96ad596
Kevin Qu <quchunguang@gmail.com>
2025-07-12 20:46:53 +0800

    add <Fx> hotkeys and fix PS1

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,3 +3,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<F2>", "<Cmd>Telescope oldfiles<CR>", { desc = "Find old files" })
+map("n", "<F3>", "<Cmd>Telescope find_files<CR>", { desc = "Find files"})
+map("n", "<F5>", "<Cmd>!python %<CR>", { desc = "Run file with python" })
+
